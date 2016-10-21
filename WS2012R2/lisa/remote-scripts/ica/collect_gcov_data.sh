bfb88963ae44bc9a5bc5cfe93d59be71b0215224
Your Name <you@example.com>
2016-10-21 17:24:57 +0300

    Extend the gcov files to be collected

--- WS2012R2/lisa/remote-scripts/ica/collect_gcov_data.sh
+++ WS2012R2/lisa/remote-scripts/ica/collect_gcov_data.sh
@@ -30,9 +30,9 @@ DumpGcovDaemon()
     rm -f ~/.gdbinit
 }
 
-SOURCE_LOC="/home/test/linux-4.8/"
-DRIVER_GCOV_LOC="/sys/kernel/debug/gcov/home/test/linux-4.8/"
-DAEMON_GCOV_LOC="/home/test/linux-4.8/tools/hv/"
+SOURCE_LOC="/home/test/linux-4.8.3/"
+DRIVER_GCOV_LOC="/sys/kernel/debug/gcov/home/test/linux-4.8.3/"
+DAEMON_GCOV_LOC="/home/test/linux-4.8.3/tools/hv/"
 
 declare -a daemons=( 'hv_kvp_daemon' 'hv_vss_daemon' 'hv_fcopy_daemon' )
 DumpGcovDaemon "${daemons[@]}"
@@ -40,21 +40,61 @@ declare -a daemons_loc=( 'tools/hv/hv_kvp_daemon.c'
                          'tools/hv/hv_fcopy_daemon.c'
                          'tools/hv/hv_vss_daemon.c' )
 rm -f ~/gcov_data.zip
-cd ${SOURCE_LOC}
+cd ${DAEMON_GCOV_LOC}
 for daemon in "${daemons_loc[@]}"
 do
-   gcov ${SOURCE_LOC}${daemon} -o ${DAEMON_GCOV_LOC}
-   zip ~/gcov_data.zip $(basename "${daemon}").gcov
+    rm -rf *.gcov
+    gcov ${SOURCE_LOC}${daemon} -o ${DAEMON_GCOV_LOC}
+    zip ~/gcov_data.zip $(basename "${daemon}").gcov
 done
 
-declare -a drivers_loc=( 'drivers/scsi/storvsc_drv.c'
+declare -a drivers_loc=( 'drivers/hv/channel.c'
+                         # af_hvsock.c - not found on upstream
+                         'drivers/hv/channel_mgmt.c'
+                         'drivers/hv/connection.c'
+                         'drivers/hid/hid-core.c'
+                         'drivers/hid/hid-debug.c'
+                         'drivers/hid/hid-hyperv.c'
+                         'drivers/hid/hid-input.c'
+                         'drivers/hid/hv.c'
+                         #'drivers/hv/hv_balloon.c' - not instrumented
+                         #'drivers/hv/hv_compat.c' - not instrumented
+                         'drivers/hv/hv_fcopy.c'
+                         'drivers/hv/hv_kvp.c'
+                         'drivers/hv/hv_snapshot.c'
+                         'drivers/hv/hv_util.c'
+                         'drivers/hv/hv_utils_transport.c'
+                         # hvnd_addr.c - not found on upstream
+                         'drivers/input/serio/hyperv-keyboard.c'
+                         'drivers/video/fbdev/hyperv_fb.c'
+                         'drivers/net/hyperv/netvsc.c'
                          'drivers/net/hyperv/netvsc_drv.c'
-                         'drivers/hv/vmbus_drv.c'
-                         'drivers/hv/hv_util.c' )
+                         # 'drivers/infiniband/hw/cxgb4/provider.c' - not instrumented
+                         'drivers/hv/ring_buffer.c'
+                         'drivers/net/hyperv/rndis_filter.c'
+                         'drivers/scsi/storvsc_drv.c'
+                         # vmbus_rdma.c - not found on upstream )
+                         'drivers/hv/vmbus_drv.c' )
+
+declare -a lib_files=( 'hyperv.h' 'mshyperv.h' 'sync_bitops.h'
+                       'access_ok.h' 'be_byteshift.h' 'be_memmove.h'
+                       'be_struct.h' 'generic.h' 'le_byteshift.h' 'le_memmove.h'
+                       'le_struct.h' 'memmove.h' 'packed_struct.h'
+                       'af_hvsock.h' 'atomic.h' 'export.h' 'hid-debug.h' 'hid.h'
+                       'hidraw.h' 'hv_compat.h' 'rndis.h' 'hid-uuid.h'
+                       'hid.h' )
+cd ${SOURCE_LOC}
 for driver in "${drivers_loc[@]}"
 do
-   gcov ${SOURCE_LOC}${driver} -o ${DRIVER_GCOV_LOC}$(dirname "${driver}")
-   zip ~/gcov_data.zip $(basename "${driver}").gcov
+    rm -rf *.gcov
+    gcov ${SOURCE_LOC}${driver} -o ${DRIVER_GCOV_LOC}$(dirname "${driver}")
+    zip ~/gcov_data.zip $(basename "${driver}").gcov
+    for lib in "${lib_files[@]}"
+    do
+        if [ -f $(basename "${lib}").gcov ]; then
+            zip ~/gcov_data.zip $(basename "${lib}").gcov
+        fi
+    done
 done
 
 #gcovr -g -k -r . --html --html-details -o /tmp/kvp_normal.html
