1fbee5db5dbd8f85140b0a916433e2a6c4de43a3
Your Name <you@example.com>
2017-01-16 14:48:05 +0200

    Update gcov collect coverage data script

--- WS2012R2/lisa/remote-scripts/ica/collect_gcov_data.sh
+++ WS2012R2/lisa/remote-scripts/ica/collect_gcov_data.sh
@@ -1,52 +1,4 @@
 #!/bin/bash
-# Function to get dump gcov data for daemon processes
-DumpGcovDaemon()
-{
-    if [ -z "$1" ]
-      then
-        echo "Error: please specify the daemon process/es name"
-        exit
-    fi
-
-    echo "set pagination off" > ~/.gdbinit
-    for process in "$@"
-    do
-        pid=`pidof ${process}`
-        if [ -z "$pid" ]
-        then
-            echo "Error: could not find process: $process"
-            continue
-        fi
-        gcov_tmp_file=/root/gcov_tmp_${pid}
-        gcov_log_file=/root/gcov_log_${pid}
-        echo "call __gcov_flush()" > ${gcov_tmp_file}
-        echo "thread apply all call __gcov_flush()" >> ${gcov_tmp_file}
-        gdb -p ${pid} -batch -x ${gcov_tmp_file} --args ${process} > ${gcov_log_file} 2>&1
-        rm -f ${gcov_tmp_file}
-        if [ -f ${gcov_log_file} ]; then
-            rm -f ${gcov_log_file}
-        fi
-    done
-    rm -f ~/.gdbinit
-}
-
-SOURCE_LOC="/home/test/linux-4.8.3/"
-DRIVER_GCOV_LOC="/sys/kernel/debug/gcov/home/test/linux-4.8.3/"
-DAEMON_GCOV_LOC="/home/test/linux-4.8.3/tools/hv/"
-
-declare -a daemons=( 'hv_kvp_daemon' 'hv_vss_daemon' 'hv_fcopy_daemon' )
-DumpGcovDaemon "${daemons[@]}"
-declare -a daemons_loc=( 'tools/hv/hv_kvp_daemon.c'
-                         'tools/hv/hv_fcopy_daemon.c'
-                         'tools/hv/hv_vss_daemon.c' )
-rm -f ~/gcov_data.zip
-cd ${DAEMON_GCOV_LOC}
-for daemon in "${daemons_loc[@]}"
-do
-    rm -rf *.gcov
-    gcov ${SOURCE_LOC}${daemon} -o ${DAEMON_GCOV_LOC}
-    zip ~/gcov_data.zip $(basename "${daemon}").gcov
-done
 
 declare -a drivers_loc=( 'drivers/hv/channel.c'
                          # af_hvsock.c - not found on upstream
@@ -83,18 +35,93 @@ declare -a lib_files=( 'hyperv.h' 'mshyperv.h' 'sync_bitops.h'
                        'af_hvsock.h' 'atomic.h' 'export.h' 'hid-debug.h' 'hid.h'
                        'hidraw.h' 'hv_compat.h' 'rndis.h' 'hid-uuid.h'
                        'hid.h' )
+
+SOURCE_LOC="/home/test/linux-4.9.2/"
+DRIVER_GCOV_LOC="/sys/kernel/debug/gcov/home/test/linux-4.9.2/"
+DAEMON_GCOV_LOC="/home/test/linux-4.9.2/tools/hv/"
+
+declare -a daemons=( 'hv_kvp_daemon' 'hv_vss_daemon' 'hv_fcopy_daemon' )
+declare -a daemons_loc=( 'tools/hv/hv_kvp_daemon.c'
+                         'tools/hv/hv_fcopy_daemon.c'
+                         'tools/hv/hv_vss_daemon.c' )
+
+# Function to get dump gcov data for daemon processes
+DumpGcovDaemon()
+{
+    if [ -z "$1" ]
+      then
+        echo "Error: please specify the daemon process/es name"
+        exit
+    fi
+
+    echo "set pagination off" > ~/.gdbinit
+    for process in "$@"
+    do
+        pid=`pidof ${process}`
+        if [ -z "$pid" ]
+        then
+            echo "Error: could not find process: $process"
+            continue
+        fi
+        gcov_tmp_file=/root/gcov_tmp_${pid}
+        gcov_log_file=/root/gcov_log_${pid}
+        echo "call __gcov_flush()" > ${gcov_tmp_file}
+        echo "thread apply all call __gcov_flush()" >> ${gcov_tmp_file}
+        gdb -p ${pid} -batch -x ${gcov_tmp_file} --args ${process} > ${gcov_log_file} 2>&1
+        rm -f ${gcov_tmp_file}
+        if [ -f ${gcov_log_file} ]; then
+            rm -f ${gcov_log_file}
+        fi
+    done
+    rm -f ~/.gdbinit
+}
+
+# Function to zip all gcov data files generated
+ZipAllGcov()
+{
+    index=$1
+    for driver in "${drivers_loc[@]}"
+    do
+        if [ -f $(basename "${driver}").gcov ]; then
+            ln $(basename "${driver}").gcov $(basename "${driver}")_${index}.gcov
+            zip ~/gcov_data.zip $(basename "${driver}")_${index}.gcov
+        fi
+    done
+    for lib in "${lib_files[@]}"
+    do
+        if [ -f $(basename "${lib}").gcov ]; then
+            ln $(basename "${lib}").gcov $(basename "${lib}")_${index}.gcov
+            zip ~/gcov_data.zip $(basename "${lib}")_${index}.gcov
+        fi
+    done
+    for daemon in "${daemons_loc[@]}"
+    do
+        if [ -f $(basename "${daemon}").gcov ]; then
+            ln $(basename "${daemon}").gcov $(basename "${daemon}")_${index}.gcov
+            zip ~/gcov_data.zip $(basename "${daemon}")_${index}.gcov
+        fi
+    done
+}
+
+rm -f ~/gcov_data.zip
+DumpGcovDaemon "${daemons[@]}"
+cd ${DAEMON_GCOV_LOC}
+i=1
+for daemon in "${daemons_loc[@]}"
+do
+    rm -rf *.gcov
+    gcov ${SOURCE_LOC}${daemon} -o ${DAEMON_GCOV_LOC}
+    ZipAllGcov "${i}"
+    i=$(($i+1))
+done
+
 cd ${SOURCE_LOC}
 for driver in "${drivers_loc[@]}"
 do
     rm -rf *.gcov
     gcov ${SOURCE_LOC}${driver} -o ${DRIVER_GCOV_LOC}$(dirname "${driver}")
-    zip ~/gcov_data.zip $(basename "${driver}").gcov
-    for lib in "${lib_files[@]}"
-    do
-        if [ -f $(basename "${lib}").gcov ]; then
-            zip ~/gcov_data.zip $(basename "${lib}").gcov
-        fi
-    done
+    ZipAllGcov "${i}"
+    i=$(($i+1))
 done
 
 #gcovr -g -k -r . --html --html-details -o /tmp/kvp_normal.html
